8bbde56b0a53cc7a05e604069f4a0e6ddbad1c4e
Giulia Scaffino <giulia.scaffino@gmail.com>
2025-11-11 16:19:59 +0100

    old timeouts restored

--- chain/src/bin/validator.rs
+++ chain/src/bin/validator.rs
@@ -34,8 +34,8 @@ const BACKFILL_BY_DIGEST_CHANNEL: u32 = 4;
 const TRANSACTION_CHANNEL: u32 = 5;
 const ANCESTOR_CHANNEL: u32 = 6;
 
-const LEADER_TIMEOUT: Duration = Duration::from_millis(1500);
-const NOTARIZATION_TIMEOUT: Duration = Duration::from_millis(3000);
+const LEADER_TIMEOUT: Duration = Duration::from_millis(10000);
+const NOTARIZATION_TIMEOUT: Duration = Duration::from_millis(12000);
 const NULLIFY_RETRY: Duration = Duration::from_secs(10);
 const ACTIVITY_TIMEOUT: u64 = 256;
 const SKIP_TIMEOUT: u64 = 32;
