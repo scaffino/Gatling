cf959a89b17edc16cb2d7c10e4dd569a214a7ba0
Giulia Scaffino <giulia.scaffino@gmail.com>
2026-05-04 10:28:11 +0200

    made validator timeout larger

--- chain/src/bin/validator.rs
+++ chain/src/bin/validator.rs
@@ -35,8 +35,8 @@ const BACKFILL_BY_DIGEST_CHANNEL: u32 = 4;
 const TRANSACTION_CHANNEL: u32 = 5;
 const ANCESTOR_CHANNEL: u32 = 6;
 
-const LEADER_TIMEOUT: Duration = Duration::from_millis(10000);
-const NOTARIZATION_TIMEOUT: Duration = Duration::from_millis(15000);
+const LEADER_TIMEOUT: Duration = Duration::from_millis(12500);
+const NOTARIZATION_TIMEOUT: Duration = Duration::from_millis(17500);
 const NULLIFY_RETRY: Duration = Duration::from_secs(10);
 const ACTIVITY_TIMEOUT: u64 = 256;
 const SKIP_TIMEOUT: u64 = 32;
